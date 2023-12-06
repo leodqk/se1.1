98609ca32ad775a3a727db3d11f7a5256e77417d
leodqk <duongquangkhai_t66@hus.edu.vn>
2023-12-06 21:26:59 +0700

    main-project

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,16 +1,64 @@
-import { useTheme } from "@mui/material/styles";
-import { Box, Divider, IconButton, Stack } from "@mui/material";
-import React from "react";
+import { useTheme, styled } from "@mui/material/styles";
+import { Avatar, Box, Divider, IconButton, Stack, Switch } from "@mui/material";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import { Nav_Buttons } from "../../data";
 import { Gear } from "phosphor-react";
+import { faker } from "@faker-js/faker";
+import useSettings from "../../hooks/useSettings";
+
+const AntSwitch = styled(Switch)(({ theme }) => ({
+  width: 28,
+  height: 16,
+  padding: 0,
+  display: "flex",
+  "&:active": {
+    "& .MuiSwitch-thumb": {
+      width: 15,
+    },
+    "& .MuiSwitch-switchBase.Mui-checked": {
+      transform: "translateX(9px)",
+    },
+  },
+  "& .MuiSwitch-switchBase": {
+    padding: 2,
+    "&.Mui-checked": {
+      transform: "translateX(12px)",
+      color: "#fff",
+      "& + .MuiSwitch-track": {
+        opacity: 1,
+        backgroundColor: theme.palette.mode === "dark" ? "#177ddc" : "#1890ff",
+      },
+    },
+  },
+  "& .MuiSwitch-thumb": {
+    boxShadow: "0 2px 4px 0 rgb(0 35 11 / 20%)",
+    width: 12,
+    height: 12,
+    borderRadius: 6,
+    transition: theme.transitions.create(["width"], {
+      duration: 200,
+    }),
+  },
+  "& .MuiSwitch-track": {
+    borderRadius: 16 / 2,
+    opacity: 1,
+    backgroundColor:
+      theme.palette.mode === "dark"
+        ? "rgba(255,255,255,.35)"
+        : "rgba(0,0,0,.25)",
+    boxSizing: "border-box",
+  },
+}));
 
 const DashboardLayout = () => {
   const theme = useTheme();
+  const [selected, setSelected] = useState(0);
+  const { onToggleMode } = useSettings();
   return (
     <>
       <Box
-        p={"16px"}
+        p={"20px"}
         sx={{
           backgroundColor: theme.palette.background.paper,
           boxShadow: "0px 0px 4px 0px rgba(0, 0, 0, 0.25)",
@@ -22,27 +70,114 @@ const DashboardLayout = () => {
           sx={{
             direction: "column",
             alignItems: "center",
+            justifyContent: "space-between",
+            height: "100%",
             width: "100%",
             gap: "48px",
           }}
         >
-          <Box
+          <Stack
             sx={{
-              backgroundColor: theme.palette.primary.main,
-              height: 64,
-              width: 64,
-              borderRadius: "12px",
+              direction: "column",
+              alignItems: "center",
+              width: "100%",
+              gap: "48px",
             }}
-          ></Box>
+          >
+            <Box
+              sx={{
+                backgroundColor: theme.palette.primary.main,
+                height: 64,
+                width: 64,
+                borderRadius: "12px",
+              }}
+            ></Box>
 
-          <Stack sx={{ direction: "column", gap: "48px" }}>
-            {Nav_Buttons.map((el) => (
-              <IconButton key={el.index}>{el.icon}</IconButton>
-            ))}
-            <Divider />
-            <IconButton>
-              <Gear></Gear>
-            </IconButton>
+            <Stack
+              sx={{ direction: "column", gap: "20px", width: "max-content" }}
+            >
+              {Nav_Buttons.map((el) =>
+                el.index === selected ? (
+                  <Box
+                    p={1}
+                    sx={{
+                      backgroundColor: theme.palette.primary.main,
+                      borderRadius: "12px",
+                    }}
+                  >
+                    <IconButton
+                      sx={{ color: "#fff", width: "max-content" }}
+                      key={el.index}
+                    >
+                      {el.icon}
+                    </IconButton>
+                  </Box>
+                ) : (
+                  <Box
+                    onClick={() => {
+                      setSelected(el.index);
+                    }}
+                    p={1}
+                    sx={{
+                      backgroundColor: theme.palette.background.paper,
+                      borderRadius: "12px",
+                    }}
+                  >
+                    <IconButton
+                      sx={{
+                        color:
+                          theme.palette.mode === "light"
+                            ? "#000"
+                            : theme.palette.text.primary,
+                        width: "max-content",
+                      }}
+                      key={el.index}
+                    >
+                      {el.icon}
+                    </IconButton>
+                  </Box>
+                )
+              )}
+              <Divider sx={{ width: "48px" }} />
+              {selected == 3 ? (
+                <Box
+                  p={1}
+                  sx={{
+                    backgroundColor: theme.palette.primary.main,
+                    borderRadius: "12px",
+                  }}
+                >
+                  <IconButton
+                    sx={{
+                      color:
+                        theme.palette.mode === "light"
+                          ? "#000"
+                          : theme.palette.text.primary,
+                    }}
+                  >
+                    <Gear></Gear>
+                  </IconButton>
+                </Box>
+              ) : (
+                <IconButton
+                  onClick={() => {
+                    setSelected(3);
+                  }}
+                >
+                  <Gear></Gear>
+                </IconButton>
+              )}
+            </Stack>
+          </Stack>
+
+          <Stack sx={{ alignItems: "center", gap: "20px" }}>
+            <AntSwitch
+              onChange={() => {
+                onToggleMode();
+              }}
+              defaultChecked
+            />
+            <Avatar src={faker.image.avatar()}></Avatar>
           </Stack>
         </Stack>
       </Box>
--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,12 +1,7 @@
 import React from "react";
 
 const GeneralApp = () => {
-
-  return (
-    <>
-      App
-    </>
-  );
+  return <></>;
 };
 
 export default GeneralApp;
