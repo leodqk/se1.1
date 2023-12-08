554287b9485dea9c8525bd72d2d48077f0bd6c31
Hoan Doan <hoandoan288@gmail.com>
2023-12-08 18:36:41 +0700

    Them khung chat

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,16 +1,26 @@
 import React from "react";
 import Chats from "./Chats";
+import { Box, Stack } from "@mui/material";
+import Conversation from "../../components/Conversation";
+import { useTheme } from "@mui/material/styles";
 
 const GeneralApp = () => {
-
+  const theme = useTheme();
   return (
-    <>
-      <Chats />
-    </>
-  );
-=======
-  return <></>;
+    <Stack direction={"row"} sx={{ width: "100%" }}>
 
+      <Chats />
+
+      <Box sx={{
+        height: "100%",
+        width: "calc(100vw - 424px)",
+        backgroundColor: theme.palette.mode === "light" ? "#fff" : theme.palette.background.default
+      }}>
+
+        <Conversation />
+      </Box>
+    </Stack >
+  );
 };
 
 export default GeneralApp;
