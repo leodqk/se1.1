ade677347410768049b0448ea6f63eb8b97a936d
kienle0109 <kienle20034@gmail.com>
2023-12-10 03:15:54 +0700

    Kien_them_chat_screen

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -8,18 +8,21 @@ const GeneralApp = () => {
   const theme = useTheme();
   return (
     <Stack direction={"row"} sx={{ width: "100%" }}>
-
       <Chats />
 
-      <Box sx={{
-        height: "100%",
-        width: "calc(100vw - 424px)",
-        backgroundColor: theme.palette.mode === "light" ? "#fff" : theme.palette.background.default
-      }}>
-
+      <Box
+        sx={{
+          height: "100%",
+          width: "calc(100vw - 424px)",
+          backgroundColor:
+            theme.palette.mode === "light"
+              ? "#F0F4FA"
+              : theme.palette.background.default,
+        }}
+      >
         <Conversation />
       </Box>
-    </Stack >
+    </Stack>
   );
 };
 
